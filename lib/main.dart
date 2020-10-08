af21a68688480dcb67c0ecbfada9251a25e7de25
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-10-08 16:23:07 +0700

    Handle scrollview when show keybroad :hammer:

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -62,79 +64,97 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double appBarHeight = AppBar().preferredSize.height;
+    final screenHeigh = MediaQueryData.fromWindow(window);
+
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(40.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(height: 100.0, child: Image.network(_image)),
-                Container(
-                  child:
-                      Text('Welcome to MFF', style: TextStyle(fontSize: 30.0)),
-                ),
-                Container(
-                  child: Text('Login',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 30.0)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 60.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: '',
-                      labelText: 'Username',
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Container(
+            child: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () => {print('Click on back press!')},
+          color: Colors.white,
+        )),
+      ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+            height: screenHeigh.size.height - (appBarHeight + 25),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.transparent,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(height: 100.0, child: Image.network(_image)),
+                  Container(
+                    child: Text('Welcome to MFF',
+                        style: TextStyle(fontSize: 30.0)),
+                  ),
+                  Container(
+                    child: Text('Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30.0)),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 60.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: '',
+                        labelText: 'Username',
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 5.0),
-                  child: TextField(
-                    decoration:
-                        InputDecoration(hintText: '', labelText: 'Password'),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: TextField(
+                      decoration:
+                          InputDecoration(hintText: '', labelText: 'Password'),
+                    ),
                   ),
-                ),
-                Container(
-                    margin: EdgeInsets.only(top: 40.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        print('Login was tapped!');
-                      },
-                      child: Container(
-                        height: 50.0,
-                        color: Color.fromRGBO(105, 175, 247, 1),
-                        child: Center(
-                          child: Text(
-                            'Login',
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 20.0),
+                  Container(
+                      margin: EdgeInsets.only(top: 40.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          print('Login was tapped!');
+                        },
+                        child: Container(
+                          height: 50.0,
+                          color: Color.fromRGBO(105, 175, 247, 1),
+                          child: Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20.0),
+                            ),
                           ),
                         ),
-                      ),
-                    )),
-                Container(
-                    margin: EdgeInsets.only(top: 10.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        print('ForgotPassword? was tapped!');
-                      },
-                      child: Container(
-                        height: 15.0,
-                        child: Center(
-                          child: Text(
-                            'Forgot Password?',
-                            style:
-                                TextStyle(color: Colors.black, fontSize: 15.0),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          print('ForgotPassword? was tapped!');
+                        },
+                        child: Container(
+                          height: 15.0,
+                          child: Center(
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 15.0),
+                            ),
                           ),
                         ),
-                      ),
-                    ))
-              ]),
-        ));
+                      ))
+                ]),
+          ),
+        ),
+      ),
+    );
   }
 }
