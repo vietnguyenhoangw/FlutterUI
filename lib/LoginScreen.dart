b44dc9094bb67991531cac5d1a2a131d4c43a3a5
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-10-11 12:38:59 +0700

    Split and add new login screen :apple:

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,110 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  LoginScreenState createState() {
+    return new LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final String _image =
+      'https://venturebeat.com/wp-content/uploads/2019/02/google-flutter-logo-white.png?fit=400%2C200&strip=all';
+
+  @override
+  Widget build(BuildContext context) {
+    double appBarHeight = AppBar().preferredSize.height;
+    final screenHeigh = MediaQueryData.fromWindow(window);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('LoginScreen'),
+        leading: Container(
+            child: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () => Navigator.pop(context),
+          color: Colors.white,
+        )),
+      ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+            height: screenHeigh.size.height - (appBarHeight + 25),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.transparent,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(height: 100.0, child: Image.network(_image)),
+                  Container(
+                    child: Text('Welcome to MFF',
+                        style: TextStyle(fontSize: 30.0)),
+                  ),
+                  Container(
+                    child: Text('Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30.0)),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 60.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: '',
+                        labelText: 'Username',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: TextField(
+                      decoration:
+                          InputDecoration(hintText: '', labelText: 'Password'),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 40.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          print('Login was tapped!');
+                        },
+                        child: Container(
+                          height: 50.0,
+                          color: Color.fromRGBO(105, 175, 247, 1),
+                          child: Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20.0),
+                            ),
+                          ),
+                        ),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          print('ForgotPassword? was tapped!');
+                        },
+                        child: Container(
+                          height: 15.0,
+                          child: Center(
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 15.0),
+                            ),
+                          ),
+                        ),
+                      ))
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
